9841a51dac2a2979057db58be5e3cc8ab7158e83
Administrator <Administrator@JKHJKL>
2019-12-26 08:18:05 +0800

    添加项目文件。

---
+++ ConsoleApplication8/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication8
+{
+    class Program
+    {
+        struct teststruct
+        {
+            public string firstname, lastname;
+            public string name() => firstname + " " + lastname;
+            
+        }
+        static void Main(string[] args)
+        {
+            teststruct mystruct = new teststruct();
+            mystruct.firstname = "Marco";
+            mystruct.lastname = "Reus";
+            Console.WriteLine(mystruct.name());
+            Console.ReadKey();
+        }
+    }
+}
